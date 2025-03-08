62104fcf324328a2ba734f19326673dd3b026e9a
gbgp2 <bernieparcon001@gmail.com>
2025-03-09 00:08:41 +0700

    new file:   404/index.html 	deleted:    b.blog.techbernme/educ/index.html 	modified:   b.blog.techbernme/index.html 	deleted:    b.blog.techbernme/lang/index.html 	new file:   b.blog.techbernme/p-aaaa-educ-20250308213301-postname copy.html 	new file:   b.blog.techbernme/p-aaaa-educ-20250308213301-postname-content copy.html 	renamed:    b.blog.techbernme/tech/main.html -> b.blog.techbernme/p-aaaa-lang-20250308213302-postname copy.html 	new file:   b.blog.techbernme/p-aaaa-lang-20250308213302-postname-content copy.html 	new file:   b.blog.techbernme/p-aaaa-tech-20250308213300-postname copy.html 	renamed:    b.blog.techbernme/tech/main_center.html -> b.blog.techbernme/p-aaaa-tech-20250308213300-postname-content copy.html 	new file:   b.blog.techbernme/p-educ-20250308213301-postname-content.html 	new file:   b.blog.techbernme/p-educ-20250308213301-postname.html 	new file:   b.blog.techbernme/p-lang-20250308213302-postname-content.html 	new file:   b.blog.techbernme/p-lang-20250308213302-postname.html 	new file:   b.blog.techbernme/p-tech-20250308213300-postname-content.html 	new file:   b.blog.techbernme/p-tech-20250308213300-postname.html 	deleted:    b.blog.techbernme/post-templates/index.html 	deleted:    b.blog.techbernme/tech/index.html 	modified:   index.html 	deleted:    permalink/index.html 	modified:   z.resources/css/main.css 	modified:   z.resources/css/main.css.map 	modified:   z.resources/css/main.scss 	modified:   z.resources/js/html-include.js 	new file:   z.resources/js/permalink-array.js

--- z.resources/js/html-include.js
+++ z.resources/js/html-include.js
@@ -29,4 +29,3 @@ function includeHTML() {
       }
     }
   }
-  includeHTML();
---
+++ z.resources/js/permalink-array.js
@@ -0,0 +1,6 @@
+const permalinks = {
+    "20250308213300": "./b.blog.techbernme/?post=p-tech-20250308213300-postname",
+    "20250308213301": "./b.blog.techbernme/?post=p-educ-20250308213301-postname",
+    "20250308213302": "./b.blog.techbernme/?post=p-lang-20250308213302-postname"
+};
+  
